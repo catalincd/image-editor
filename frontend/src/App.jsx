186a1bc57ae95ede6c4ac74428db1637ffdcb622
Hinoveanu Razvan <razvan.hinoveanu02@e-uvt.ro>
2024-05-30 15:29:47 -0400

    Reparatii icoana site html/jsx d-ala

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
     <ImageContext.Provider value={{ image, setImage }}>
       <div className={`App ${darkMode ? 'bg-dark text-white' : 'bg-light'}`}>
         <div className="top-toolbar d-flex justify-content-between align-items-center px-3 py-2">
-          <img src="static/logo.png" alt="Logo" className="App-logo" onClick={resetAndGoHome} style={{ width: '240px', height: '70px'}} />
+          <img src="logo.png" alt="Logo" className="App-logo" onClick={resetAndGoHome} style={{ width: '240px', height: '70px'}} />
           <button onClick={toggleDarkMode} className={`btn btn-${darkMode ? 'light' : 'secondary'} ml-auto`}>
             {darkMode ? 'Light Mode' : 'Dark Mode'}
           </button>
